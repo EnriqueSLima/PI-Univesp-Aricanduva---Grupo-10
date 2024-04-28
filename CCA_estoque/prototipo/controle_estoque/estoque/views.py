d5f8607158222c6e480cf924b67c25db98b7a934
snowfox <souza.enrique7@gmail.com>
2024-04-28 16:29:09 -0300

    Prototipo descontinuado

--- CCA_estoque/prototipo/controle_estoque/estoque/views.py
+++ CCA_estoque/prototipo/controle_estoque/estoque/views.py
@@ -8,29 +8,39 @@ def index(request):
     return render(request, 'estoque/index.html')
 
 def inserir(request):
+    context = {
+        'tipos' : Tipo.objects.all(),
+        'tamanhos' : Tamanho.objects.all(),
+        'cores' : Core.objects.all(),
+        'qtd' : Uniforme.uquantidade,
+    }
     if request.method == 'GET':
-        context = {
-            'tipos' : Tipo.objects.all(),
-            'tamanhos' : Tamanho.objects.all(),
-            'cores' : Core.objects.all(),
-            'qtd' : Uniforme.uquantidade,
-        }
+        return render(request, 'estoque/inserir.html', context)
+    # Caso o método for POST 
+    else:
+        #TODO: Implementar alteração no banco de dados
+        tipo = request.POST.get('idutipo')
+        tamanho = request.POST.get('idutamanho')
+        cor = request.POST.get('iducor')
+        qtd = request.POST.get('iduquantidade')
+
         return render(request, 'estoque/inserir.html', context)
 
 def remover(request):
+    context = {
+        'tipos' : Tipo.objects.all(),
+        'tamanhos' : Tamanho.objects.all(),
+        'cores' : Core.objects.all(),
+        'qtd' : Uniforme.uquantidade,
+    }
     if request.method == 'GET':
-        context = {
-            'tipos' : Tipo.objects.all(),
-            'tamanhos' : Tamanho.objects.all(),
-            'cores' : Core.objects.all(),
-            'qtd' : Uniforme.uquantidade,
-        }
         return render(request, 'estoque/remover.html', context)
 
 def consulta(request):
+    context = {
+        'uniformes' : Uniforme.objects.all()
+    }
     if request.method == 'GET':
-        context = {
-            'uniformes' : Uniforme.objects.all()
-        }
-        #uniformes = Uniforme.objects.all()
+        return render(request, 'estoque/consulta.html', context)
+    else:
         return render(request, 'estoque/consulta.html', context)
