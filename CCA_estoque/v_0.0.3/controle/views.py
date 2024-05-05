2239babcc2d38bfa9b36a78e66d0a084f47a1181
snowfox <souza.enrique7@gmail.com>
2024-05-05 02:03:17 -0300

    Adicionando formulários de inserção e remoção

--- CCA_estoque/v_0.0.3/controle/views.py
+++ CCA_estoque/v_0.0.3/controle/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from .models import Tipo, Estilo, Core, Tamanho, Uniforme
 
+context = {
+        'tipos' : Tipo.objects.all(),
+        'estilos' : Estilo.objects.all(),
+        'cores' : Core.objects.all(),
+        'tamanhos' : Tamanho.objects.all(),
+        'qtd' : Uniforme.qtd,
+    }
 
 # AUTENTIFICAÇÃO REQUERIDA
 @login_required(login_url = 'autenticar')
@@ -10,13 +18,12 @@ def home(request):
 
 @login_required(login_url = 'autenticar')
 def inserir(request):
-    return render(request, 'inserir.html')
+    return render(request, 'inserir.html', context)
 
 @login_required(login_url = 'autenticar')
 def remover(request):
-    return render(request, 'remover.html')
-
+    return render(request, 'remover.html', context)
 
 @login_required(login_url = 'autenticar')
 def consultar(request):
-    return render(request, 'consultar.html')
+    return render(request, 'consultar.html', context)
