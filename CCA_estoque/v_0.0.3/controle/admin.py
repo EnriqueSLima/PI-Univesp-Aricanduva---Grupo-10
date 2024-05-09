798ffecd7ba78961154d912e1dc18a4f88878620
snowfox <souza.enrique7@gmail.com>
2024-05-09 00:22:15 -0300

    Deletando dados de versões antigas

--- CCA_estoque/v_0.0.3/controle/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Tipo, Estilo, Core, Tamanho, Uniforme
-
-admin.site.register(Tipo)
-admin.site.register(Estilo)
-admin.site.register(Core)
-admin.site.register(Tamanho)
-admin.site.register(Uniforme)
-
--- CCA_estoque/v_0.0.4/autenticar/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('auth/', views.autenticar, name='autenticar'),
-    path('cadastrar/', views.cadastrar, name='cadastrar'),
-]
