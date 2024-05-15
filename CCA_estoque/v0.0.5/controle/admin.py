3437f02e04268de553006d7d05a5c09d25d31b0e
snowfox <souza.enrique7@gmail.com>
2024-05-15 18:28:27 -0300

    Descontinuando v0.0.5

--- CCA_estoque/v0.0.5/controle/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Tipo, Core, Tamanho, Uniforme
-
-admin.site.register(Tipo)
-admin.site.register(Core)
-admin.site.register(Tamanho)
-admin.site.register(Uniforme)
-
